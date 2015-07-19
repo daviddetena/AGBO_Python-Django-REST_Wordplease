8bd7b0b1ad94a4e72c7347dd6ae9c6ecb4a52eb1
David de Tena <daviddetenacalvo@gmail.com>
2015-07-19 21:47:10 +0200

    Replaced html code in blogs.views with a template for home url. Posts printed at home url are now ordered by the <created_at> field

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,4 @@
 # -* encoding:utf-8 *-
-from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from blogs.models import Post
 
@@ -8,12 +7,18 @@ from blogs.models import Post
 def home(request):
     """
     Vista que se muestra para el directorio raíz de la plataforma
-    :param request: Método con el objeto request de la petición
-    :return: HttpReponse con el código html que se entregará al usuario
+    :param request: Objeto request de la petición
+    :return: Objeco HttpResponse con el código html que se entregará al usuario
     """
-    posts = Post.objects.all()
-    html = '<ul>'
-    for post in posts:
-        html += '<li>' + post.title + '</li>'
-    html += '</ul>'
-    return HttpResponse(html)
+    # A través del object manager de clase <objects> obtenemos los objetos del modelo Post. Configura la query
+    posts = Post.objects.all().order_by('-created_at')
+
+    # El context es lo que se le pasará al template, siendo las claves del diccionario accesibles desde ellas
+    context = {
+        # No carga en memoria todos los objetos, sino los 5 primeros. Aquí pondría el LIMIT X de SQL
+        # Sólo en el momento en que la variable va a ser utilizada es traído de la DB
+        "post_list": posts[:5]
+    }
+
+    # Con el render hacemos que nos pinte la template indicada en el 2º param, que es un html
+    return render(request, 'blogs/home.html', context)
