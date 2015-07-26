f61cff910c95f1779d872752bba613892362a744
David de Tena <daviddetenacalvo@gmail.com>
2015-07-26 10:45:00 +0200

    Custom Django Admin for Post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,52 @@
+# -* encoding:utf-8 *-
 from django.contrib import admin
 from posts.models import Post
 
-# Register my models for Admin UI
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    """
+
+    """
+    # Lista de columnas que se verán en el admin. Para el blog llamamos a método que me devuelve el nombre y apellidos del usuario propietario del blog
+    list_display = ('title', 'summary', 'blog_owner_name', 'created_at', 'published_at')
+
+    # Lista de filtros que aparecerán a la derecha
+    list_filter = ('blog',)
+
+    # Lista de parámetros por los que se podrá buscar
+    search_fields = ('title', 'summary', 'body')
+
+
+    def blog_owner_name(self, obj):
+        """
+        Método que altera el campo list_display blog para poner el nombre y apellidos del propietario del blog del post
+        :param obj:
+        :return:
+        """
+        return obj.blog.owner.first_name + u' ' + obj.blog.owner.last_name
+
+    # Atributos a métodos de función blog_owner_name, para poder indicar el header de columna y el campo correspondiente por el que ordena
+    blog_owner_name.short_description = u'Blog owner'
+    blog_owner_name.admin_order_field = 'blog'
+
+
+    # Definimos campos que aparecen en la vista detalle del Post en el admin
+    fieldsets = (
+        ('General', {
+            'fields': ('title', 'summary', 'body'),
+            'classes': ('wide',)
+        }),
+        ('Addional info',{
+            'fields': ('imageUrl', 'blog', 'categories'),
+            'classes': ('wide',)
+        }),
+        ('Dates',{
+            'fields': ('published_at',),
+            'classes': ('wide', 'collapse')
+        })
+    )
+
+
+
+# Registramos modelo Post, manejado por nuestra clase PostAdmin
+admin.site.register(Post, PostAdmin)
